295ca6badcde425fd21ea1c56991ffd6f09cc16a
jeremy-lee02 <s3752975@rmit.edu.vn>
2021-09-17 19:11:42 +0700

    Update heroku

--- index.js
+++ index.js
@@ -48,10 +48,14 @@ app.get('/users', function(req, res){
         res.send(forms)
     })
  })
- app.use(express.static(path.resolve(__dirname, './client/build')));
- app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-   });
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.resolve(__dirname, './client/build')));
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+      });
+}
+
 
 
     
