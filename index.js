d36806fd564ddee4ac7dd3b4fc4b75a903b31c5a
jeremy-lee02 <s3752975@rmit.edu.vn>
2021-09-17 19:39:06 +0700

    Update build

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ connectDB()
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 
 app.use('/', authRouter)
@@ -49,12 +50,9 @@ app.get('/users', function(req, res){
     })
  })
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.resolve(__dirname, './client/build')));
-    app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-      });
-}
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  });
 
 
 
