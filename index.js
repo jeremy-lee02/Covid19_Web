5934951df63fa8f7aef76046bb0bbc747f7783a2
annguyen1909 <72086169+annguyen1909@users.noreply.github.com>
2021-09-18 18:00:34 +0700

    fix

--- index.js
+++ index.js
@@ -25,13 +25,22 @@ const connectDB = async()=> {
 // const users= mongoose.model('users')
 
 
-
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, 'Client/build')));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
+    });
+}else{
+    app.get('/',(req,res)=>{
+        res.send('Api running');
+    });
+}
 
 connectDB()
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, './Client/build')));
 
 
 app.use('/', authRouter)
@@ -51,7 +60,7 @@ app.get('/users', function(req, res){
  })
 
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+   res.sendFile(path.resolve(__dirname, 'Client','build','index.html'));
   });
 
 
